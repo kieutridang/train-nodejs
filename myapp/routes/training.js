859f58757b2a1091241648aa81f46b3d98e970f5
Lê Trần Ngọc Anh <anh.le.mmo@gmail.com>
2019-11-15 07:24:58 +0700

    3-Add task

--- myapp/routes/training.js
+++ myapp/routes/training.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET html index file */
+var taskList = [];
+
+/* POST task to array */
+router.post('/addtask', function(req, res, next) {
+  var item = req.body.item;
+  taskList.push({taskName: item, taskId: Date.now(), isDone: 'false'});
+  res.redirect('/');
+});
+
+/* GET task */
 router.get('/', function(req, res, next) {
-  var options = {
-    root: 'public/html'
-  }
-  res.sendFile('index.html', options);
+  res.render('tasklist', { taskList: taskList });
 });
 
 module.exports = router;
