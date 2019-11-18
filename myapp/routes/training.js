d77778d89112632cbd8c8fbfb1696f2fe7a73e6d
Lê Trần Ngọc Anh <anh.le.mmo@gmail.com>
2019-11-18 11:53:55 +0700

    Fix: correct coding rule and use array method

--- myapp/routes/training.js
+++ myapp/routes/training.js
@@ -4,28 +4,21 @@ var router = express.Router();
 var taskList = [];
 
 /* POST task to array */
-router.post('/addtask', function(req, res, next) {
+router.post('/add-task', function(req, res, next) {
   var item = req.body.item;
   taskList.push({taskName: item, taskId: Date.now(), isDone: 'false'});
   res.redirect('/');
 });
 
 /* GET all tasks */
-router.get('/getalltasks', function(req, res, next) {
+router.get('/get-all-tasks', function(req, res, next) {
   res.send({taskList});
 });
 
 /* GET specific task by ID*/
-router.get('/searchtaskbyid', function(req, res, next) {
-  console.dir(req.query.taskId);
-  var searchId = req.query.taskId;
-  for (var i=0; i<taskList.length; i++) {
-    if (taskList[i].taskId == searchId) {
-      var taskFound = taskList[i].taskName;
-      res.send('Task found: ' + taskFound);
-    };
-  };
-  res.send('No task found !!!');
+router.get('/search-task-by-id', function(req, res, next) {
+  const taskFound = taskList.find( task => task.taskId == req.query.taskId);
+  res.send('Task found: ' + taskFound.taskName);
 });
 
 module.exports = router;
