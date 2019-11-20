df54b5806a23bc5db25eee3d8eabcc0fe73cff3f
Lê Trần Ngọc Anh <anh.le.mmo@gmail.com>
2019-11-20 20:07:01 +0700

    Feat: edit and delete task
    Feat: edit and delete task

--- myapp/routes/training.js
+++ myapp/routes/training.js
@@ -3,6 +3,23 @@ var router = express.Router();
 
 var taskList = [];
 
+function editTask (tasklist, id, newTask) {
+  var position = tasklist.findIndex(task => task.taskId == id);
+  if(position === -1) {
+          return null;
+  }
+  taskList[position].taskName = newTask;
+  return taskList
+}
+
+function deleteTask (tasklist, id) {
+  var position = tasklist.findIndex(task => task.taskId == id);
+  if(position === -1) {
+          return null;
+  }
+  return tasklist.splice(position,1);
+}
+
 /* POST task to array */
 router.post('/add-task', function(req, res, next) {
   var item = req.body.item;
@@ -19,6 +36,18 @@ router.get('/get-all-tasks', function(req, res, next) {
 router.get('/search-task-by-id', function(req, res, next) {
   const taskFound = taskList.find( task => task.taskId == req.query.taskId);
   res.send('Task found: ' + taskFound.taskName);
+});
+
+/* EDIT task */
+router.post('/edit-task', function(req, res, next) {
+  editTask(taskList,req.query.taskId,req.body.item);
+  res.redirect('/');
+});
+
+/* DELETE task*/
+router.delete('/delete-task', function(req, res, next) {
+  deleteTask(taskList,req.query.taskId);
+  res.redirect('/');
 });
 
 module.exports = router;
